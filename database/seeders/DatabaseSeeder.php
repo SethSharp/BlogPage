7068ffcd89cfd4f259c7eb8ab492b78760b69af0
Seth Sharp <sesharp@outlook.com>
2023-09-04 09:50:34 +1000

    rough seeding fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use App\Models\Comment;
 use Illuminate\Database\Seeder;
 use \App\Models\Blog;
+use Illuminate\Support\Facades\Hash;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -19,13 +21,18 @@ class DatabaseSeeder extends Seeder
 
         $first = User::factory()->create([
             'name' => 'James',
-            'role' => 'admin'
+            'role' => 'admin',
+            'email' => 'james@blog.test',
+            'password' => Hash::make('123456')
         ]);
         Blog::factory(4)->create([
             'user_id' => $first->id
         ]);
         $second = User::factory()->create([
             'name' => 'Glyb',
+            'role' => 'admin',
+            'email' => 'glyb@blog.test',
+            'password' => Hash::make('123456')
         ]);
         Blog::factory(4)->create([
             'user_id' => $second->id
@@ -35,7 +42,8 @@ class DatabaseSeeder extends Seeder
             $x = rand(1,4);
             for ($i = 0; $i < $x; $i++) {
                 Comment::factory()->create([
-                    'blog_id' => $blog->id
+                    'blog_id' => $blog->id,
+                    'user_id' => $second->id,
                 ]);
             }
         }
